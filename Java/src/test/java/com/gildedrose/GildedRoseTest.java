892d71ba5fe34f6ff8af5168e4e85e131a03fb9a
Mathias <mpenavai@enssat.fr>
2024-12-05 14:35:58 +0000

    Delete all of all unused files

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
@@ -11,7 +12,7 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
     @Test
@@ -21,4 +22,15 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals("fixme", app.items[0].name,"yeah");
     }
+
+    @Test
+    void alwaysPasses() {
+        assertTrue(true, "Ce test doit toujours réussir");
+    }
+
+    @Test
+    void alwaysFails() {
+        assertTrue(false, "Ce test doit toujours échouer");
+    }
+    
 }
