062ddc38415353078e3a4f595091970920140975
Mathias <mpenavai@enssat.fr>
2025-01-29 19:33:32 +0100

    the build success

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,12 +21,13 @@ class GildedRose
     public static void main(String[] args){
         int[][] qualityEvolutionRatesTable = { {20, 1}, {0, -1} };
 
-        List<Item> items = new ArrayList<>();
-        items.add(new ClassicItem("Ketchup", 15, 35));
-        items.add(new DynamicQualityItem("Aged Brie", 20, qualityEvolutionRatesTable, 35));
-        items.add(new LegendaryItem("Sulfuras, Hand of Ragnaros", 15, 80));
+        Item[] items = new Item[]{
+            new ClassicItem("Ketchup", 15, 35),
+            new DynamicQualityItem("Aged Brie", 20, qualityEvolutionRatesTable, 35),
+            new LegendaryItem("Sulfuras, Hand of Ragnaros", 15, 80),
+        };
 
-        GildedRose gildedrose = new GildedRose(items);
+        GildedRose gildedRose = new GildedRose(items);
         for (int i=0; i<4; i++){
             gildedRose.update();
         }
@@ -34,88 +35,4 @@ class GildedRose
 
     
 
-}                
-            /*
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;git a
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-            */
-        
- 
-
-
-/*
- * 
- * 
- *       if (!items[i].isPerishable) {
-            items[i].sellIn = items[i].sellIn - 1;
-            if (items[i].sellIn == 0){
-                items[i].quality = items[i].quality - 1;
-            }
-        }
-        
-        if (items[i].quality < 50 && items[i].quality > 0) {
-            if (items[i].sellIn <= items[i].qualityEvolutionRatesTable[0][0]){
-                int counter = 0;
-                int length = items[i].qualityEvolutionRatesTable.length;
-                while (items[i].sellIn <= items[i].qualityEvolutionRatesTable[counter+1][0]){
-                    counter ++;
-                }
-                int newQuality = items[i].quality + items[i].qualityEvolutionRatesTable[counter+1][1];
-                if (newQuality < 0){
-                    items[i].quality = 0;
-                }
-                else if (newQuality > 50){
-                    items[i].quality = 50;
-                }
-                else {
-                    items[i].quality = newQuality;
-                }
-            }
-        }
- */
+} 
